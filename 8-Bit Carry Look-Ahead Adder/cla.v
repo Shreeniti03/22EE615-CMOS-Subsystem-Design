91e25bacce14ccba2abe53ce23393c6e9ab51b99
Shreeniti03 <shreenitishastrakar@gmail.com>
2025-03-16 20:57:56 +0530

    Add files via upload

--- 8-Bit Carry Look-Ahead Adder/cla.v
+++ 8-Bit Carry Look-Ahead Adder/cla.v
@@ -1,1 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.03.2025 11:41:01
+// Design Name: 
+// Module Name: cla
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CarryLookAheadAdder(a, b, cin, sum, cout);
+    input [7:0] a, b;
+    input cin;
+    output [7:0] sum;
+    output cout;
+    wire [7:0] p, g, c;
+    assign p = a ^ b;
+    assign g = a & b;
+    assign c[0] = cin;
+    assign c[1] = g[0] | (p[0] & c[0]);
+    assign c[2] = g[1] | (p[1] & c[1]);
+    assign c[3] = g[2] | (p[2] & c[2]);
+    assign c[4] = g[3] | (p[3] & c[3]);
+    assign c[5] = g[4] | (p[4] & c[4]);
+    assign c[6] = g[5] | (p[5] & c[5]);
+    assign c[7] = g[6] | (p[6] & c[6]);
+    assign cout = g[7] | (p[7] & c[7]);
+    assign sum = p ^ {c[6:0], cin};
+endmodule
 
